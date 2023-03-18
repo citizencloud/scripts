0faa0b4ac068e22bec1453cea8ca7a4d32e4fe2a
christopher whitfield <whitfiecbeta@gmail.com>
2023-03-18 07:57:49 -0700

    test github workflow

---
+++ foo/foobar.go
@@ -0,0 +1,5 @@
+package foo
+
+func zanzibar() string {
+	return "mamamia"
+}
---
+++ foo/foobar_test.go
@@ -0,0 +1,11 @@
+package foo
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Foo(t *testing.T) {
+	assert.Equal(t, zanzibar(), "mama-mia")
+}
